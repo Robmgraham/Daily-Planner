876d29dc5e6f8a276ada4d811928d127889c9866
rancid <robmgraham13@yahoo.com>
2020-08-03 10:09:18 -0700

    timeblocks inserted, save button works with local storage, initial moment added

---
+++ script.js
@@ -0,0 +1,8 @@
+//set the time using moment.js; set the h4's to change color with hour
+
+var moment = require('moment'); // require
+moment().format("LT");function(h4){
+    if ("HH<")setattribute(".future") 
+    if ("=HH")setattribute(".present")
+    if ("HH>")setattribute(".past")console.log("moment");
+};
